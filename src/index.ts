0f635b032b4fde973d04faaaaf28dc6ab8eb02d0
周炜烽 <zhouwf@dxy.cn>
2019-12-20 16:22:44 +0800

    chore: 添加headers处理

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { AxiosRequestConfig } from './types/index'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -9,11 +11,23 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformData(config: AxiosRequestConfig): any {
+  const { data } = config
+  return transformRequest(data)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
