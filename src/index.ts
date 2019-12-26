b650192e0d5f9a1af6cf66f56424be829df1185a
周炜烽 <zhouwf@dxy.cn>
2019-12-26 14:41:50 +0800

    feat: 实现错误类逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,5 @@
-import { AxiosRequestConfig, AxiosPromise } from './types/index'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
+import axios from './axios'
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformData(config)
-}
-
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformData(config: AxiosRequestConfig): any {
-  const { data } = config
-  return transformRequest(data)
-}
-
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: any): any {
-  res.data = transformResponse(res.data)
-  return res
-}
+export * from './types'
 
 export default axios
