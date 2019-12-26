5af0e80123666ad959e3cbd5aafc2ba6854cb579
周炜烽 <zhouwf@dxy.cn>
2019-12-26 18:26:41 +0800

    feat: 完成混合对象编写

---
+++ src/core/dispatchRequest.ts
@@ -0,0 +1,40 @@
+import { AxiosRequestConfig, AxiosPromise } from '../types/index'
+import xhr from './xhr'
+import { buildURL } from '../helpers/url'
+import { transformRequest, transformResponse } from '../helpers/data'
+import { processHeaders } from '../helpers/headers'
+
+export default function dispatchRequest(
+  config: AxiosRequestConfig
+): AxiosPromise {
+  processConfig(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+function transformData(config: AxiosRequestConfig): any {
+  const { data } = config
+  return transformRequest(data)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformResponseData(res: any): any {
+  res.data = transformResponse(res.data)
+  return res
+}
